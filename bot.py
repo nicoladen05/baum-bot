3e0ff851c979e464d77ea965a2d563339c1dec04
nicoladen05 <nicolashartmanntaba@gmail.com>
2020-08-06 14:39:01 +0200

    help command

--- bot.py
+++ bot.py
@@ -1,18 +1,22 @@
 import discord
 
-token = ("Token Here!")
+token = ("NTg3NTYzMjk3NjUwMjQ1NjU4.XP4Y9g.KV81IPuwzCIVLxSICNdpQ-6fdTw")
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print("Bot logged in!")
-    await client.change_presence(activity=discord.Game("Baumstamm"))
+    await client.change_presence(activity=discord.Game("Baum"))
 
 @client.event
 async def on_message(message):
     if message.content.find("!test") != -1:
         await message.channel.send("Bot funktioniert!")
+    if message.content.find("!help") != -1:
+        embed = discord.Embed(title="Baum Bot", description="Hilfe für die Commands")
+        embed.add_field(name="!test", value="Pingt den Bot")
+        await message.channel.send(content=None, embed=embed)
 
 @client.event
 async def on_member_join(member):
