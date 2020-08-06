3d5ea26f16c9951223231e016f1b0d82dc540bf2
nicoladen05 <nicolashartmanntaba@gmail.com>
2020-08-06 17:25:19 +0200

    added join and leave functions

--- bot.py
+++ bot.py
@@ -7,21 +7,35 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("Bot logged in!")
-    await client.change_presence(activity=discord.Game("Baum"))
+    await client.change_presence(activity=discord.Game("Baumstamm"))
+
 
 @client.event
 async def on_message(message):
-    if message.content.find("!test") != -1:
-        await message.channel.send("Bot funktioniert!")
     if message.content.find("!help") != -1:
-        embed = discord.Embed(title="Baum Bot", description="Hilfe für die Commands")
+        embed = discord.Embed(title="Baum Bot", description="Alle Commands für den Bot")
         embed.add_field(name="!test", value="Pingt den Bot")
         await message.channel.send(content=None, embed=embed)
 
+    elif message.content.find("!test") != -1:
+        await message.channel.send("Bot funktioniert!")
+
+
 @client.event
 async def on_member_join(member):
-    for channel in member.guild.channels:
-        if str(channel) == "neue-bäume":
-            await channel.send_message(f"""{member.mention} ist nun ein Baum!""")
+    channel = client.get_channel(560509329086611476)
+    guild=member.guild
+    message = f"{member.mention} ist nun ein Baum!"
+    await channel.send(message)
+    await member.edit(nick=f"Baum🌳🌲{member.name}", reason="Prefix edit")
+    baumrole = get(member.guild.roles, name="Baum")
+    await member.add_role(baumrole)
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(560509329086611476)
+    guild=member.guild
+    message = f"{member.mention} ist kein Baum mehr"
+    await channel.send(message)
 
 client.run(token)
